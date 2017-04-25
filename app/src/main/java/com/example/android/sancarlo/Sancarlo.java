dccb274eda9d6f5277293fdd20bd1e5fd886c0f0
redlor <redlor1983@gmail.com>
2017-04-25 08:41:49 +0200

    Initial commit

---
+++ app/src/main/java/com/example/android/sancarlo/Sancarlo.java
@@ -0,0 +1,13 @@
+package com.example.android.sancarlo;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class Sancarlo extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_sancarlo);
+    }
+}
